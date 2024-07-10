333b0637c16ded51b7b0e272704ce8f66cc14168
B1gF1s4 <120759854+B1gF1s4@users.noreply.github.com>
2024-07-10 10:20:39 +0200

    updates assembly info

--- AdditionalMiserySpawns/Properties/AssemblyInfo.cs
+++ AdditionalMiserySpawns/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using MelonLoader;
 using System.Reflection;
 
 [assembly: AssemblyTitle("AdditionalMiserySpawns")]
-[assembly: AssemblyCopyright("B1gF1s4")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
-[assembly: MelonInfo(typeof(Mod), "AdditionalMiserySpawns", "1.0.1", "B1gF1s4", null)]
+[assembly: AssemblyCopyright("BigFish")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: MelonInfo(typeof(Mod), "AdditionalMiserySpawns", "1.0.2", "BigFish", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
